aa2b94032bb52c6e239a973dfd1aefe42d77e0e6
lisandroprada <lisandro.prada@gmail.com>
2020-06-26 10:32:47 -0300

    First commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+// Escuchar sucesos
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexion con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Lisandro',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('Respuesta server:', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor: ', mensaje);
+});
